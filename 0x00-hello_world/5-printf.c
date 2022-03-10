11acc1808a51c783b9b91068651817848a889229
Oluchime Promise <thesixthking567@gmail.com>
2022-03-10 15:15:17 -0800

    5 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - entry point
- *Return: Always 0 (Success)
+ * main - entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
