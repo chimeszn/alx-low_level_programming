a5106d635adb101dfe9844bac7e646f4f61256de
Oluchime Promise <thesixthking567@gmail.com>
2022-03-10 14:21:09 -0800

    4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 /**
- * main - entry point
- * puts - prints the phrase 'Programming is like building a multilingual puzzle'
+ * main - prints the phrase 'Programming is like building a multilingual puzzle'
  * return - 0 if exited properly, non-zero if otherwise
  */
 int main(void)
