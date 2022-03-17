05402cc3c0d2f9041a5d4dd5a3acad3f3c3ec9c3
Oluchime Promise <thesixthking567@gmail.com>
2022-03-17 12:22:55 -0700

    eight commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square followed by a new line
+ * @size: size of square (length and width)
+ * @#: character to represent the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		int row, column;
+
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
