f1283d88cdd23083d9f8cf774490bb0cf75f66e0
Oluchime Promise <thesixthking567@gmail.com>
2022-03-17 09:39:49 -0700

    fifth commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '14'; j++)
+		for (j = '0'; j < '15'; j++)
 		{
-			if (j >= '10')
-				_putchar((j / '10') + '0');
-			_putchar((j % '10') + '0');
+			if (j >= 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
