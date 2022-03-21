3ef361ff6a5392740d1336c66efb8751dc5f5c8f
Oluchime Promise <thesixthking567@gmail.com>
2022-03-21 14:44:12 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints alternating chars of a string, starting with
+ * the first char, followed by a new line
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
