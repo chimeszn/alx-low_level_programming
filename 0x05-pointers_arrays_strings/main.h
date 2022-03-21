d1c2d401402b3e5abe4b9a38e705aa52a443c146
Oluchime Promise <thesixthking567@gmail.com>
2022-03-21 13:39:09 -0700

    initial commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,5 +8,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
