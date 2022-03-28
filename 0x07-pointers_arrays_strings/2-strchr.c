61acf3c5c16464940b1dc83af5e2b0ae699148d9
Oluchime Promise <thesixthking567@gmail.com>
2022-03-28 08:03:12 -0700

    initial commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,16 +12,9 @@ char *_strchr(char *s, char c)
 	unsigned int i;
 
 	for (; *(s + i) != '\0'; i++)
-	{
 		if (*(s + i) == c)
-		{
 			return (s + i);
-		}
 	if (*(s + i) == c)
-	{
 		return (s + i);
-	}
-	}
-
 	return ('\0');
 }
