594e600921556ea39bb337c0523e22b8c5cd290c
Oluchime Promise <thesixthking567@gmail.com>
2022-03-28 09:11:16 -0700

    initial commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,22 +2,23 @@
 
 /**
  * print_chessboard - prints out a chessboard
- * @a: input pointer, 2d array of chars
+ * @a: 2d array of chars
  * Return: void
  */
 
 void print_chessboard(char (*a)[8])
 {
-	int i, m = 0;
+	int i, j;
 
-	for (i = 0; i < 64; i++)
+	i = 0;
+	while (i < 8)
 	{
-		if (i % 8 == 0 && i != 0)
+		j = 0;
+		while (j < 8)
 		{
-			m = i;
-			_putchar('\n');
+			_putchar (a[i][j]);
+			j++;
 		}
-		_putchar(a[i / 8][i - m]);
+		_putchar ('\n')
 	}
-	_putchar('\n')
 }
