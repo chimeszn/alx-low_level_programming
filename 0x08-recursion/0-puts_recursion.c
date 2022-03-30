299352eaf1571a2669f800ed08f5040cf4bbb799
Oluchime Promise <thesixthking567@gmail.com>
2022-03-30 15:25:40 -0700

    initial commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,12 @@
 /**
  * _puts_recursion - prints a string followed by a new line
  * @s: string
- * Return: void
+ * Return: no return
  */
 
 void _puts_recursion(char *s)
 {
-	if (s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
