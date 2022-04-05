588bea544990dc966d1322da257ab1287e8bb927
Oluchime Promise <thesixthking567@gmail.com>
2022-04-05 13:05:14 -0700

    initial commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer of an array of characters
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *strout;
+	unsigned int i, j, k, l;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	strout + malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
+
+	for (k = 0; k < i; k++)
+	{
+		strout[k] = s1[k];
+	}
+
+	l = j;
+	for (j = 0; j <= l; k++, j++)
+	{
+		strout[k] = s2[j];
+	}
+
+	return (strout);
+}
