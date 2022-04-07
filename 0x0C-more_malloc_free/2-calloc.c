36bd7cd9acefb807ecb98d25f54154f227085a4e
Oluchime Promise <thesixthking567@gmail.com>
2022-04-07 08:31:49 -0700

    initial commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,10 +13,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int i;
 
-	if (nmemb == 0 || size ++ 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
-	if (p = NULL)
+
+	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
