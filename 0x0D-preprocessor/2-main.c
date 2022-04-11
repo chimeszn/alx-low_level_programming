5ff8f136337869f7bf9bfe560bbc704b8cb7369e
Oluchime Promise <thesixthking567@gmail.com>
2022-04-11 13:52:52 -0700

    initial commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file where the program
+ * was compiled from, followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
