bcad0502be2f5496e05a050270e8ce59dc60a5c5
Oluchime Promise <thesixthking567@gmail.com>
2022-04-13 08:26:58 -0700

    initial commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a func given as parameter on
+ * each element of an array
+ * @array: input integer array
+ * @size: size of array
+ * @action: pointer to the function
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,3 +1,5 @@
 This is a READ Me file containing what each files do
 
 1. 0-print_name.c - contains a function that prints a name using pointers to functions
+
+2. 1-array_iterator.c - contains a function that executes a function given as a parameter on each element of an array
