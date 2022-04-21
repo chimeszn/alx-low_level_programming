006e02237d6225c03f11d94906d3806fdcb4607d
Oluchime Promise <thesixthking567@gmail.com>
2022-04-21 12:24:30 -0700

    created 4-free_list.c file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "limits.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: list_t list that is freed
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
